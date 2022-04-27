5144c63257f50326c716b0563d1bed309c34b460
applemango <apple.mangoooo97@gmail.com>
2022-04-27 15:24:37 +0900

    2022/04/27 fix cpu

--- docs/main.js
+++ docs/main.js
@@ -1,10 +1,19 @@
-html_data = "<body><div id='main'><div id='board'><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div></div><div></div></body>";
+html_data = "<head><meta name='viewport' content='width=device-width, initial-scale=1.0'><link rel='stylesheet' href='stylesheet.css'><meta charset='utf-8'><title>tic toc toe</title><head><body><div id='main'><div id='board'><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div></div><div></div></body>";
 document.querySelector("html").innerHTML += html_data;
-let board = [0,0,0,0,0,0,0,0,0];
-//const board_element_main = document.querySelector("#board");
 const board_element = document.querySelectorAll("#board > div");
 const press_space_key = document.querySelector("body > div:nth-child(2)")
 document.addEventListener('keydown', keydown);
+board_element[0].addEventListener("click", function(e){player_click(0);});
+board_element[1].addEventListener("click", function(e){player_click(1);});
+board_element[2].addEventListener("click", function(e){player_click(2);});
+board_element[3].addEventListener("click", function(e){player_click(3);});
+board_element[4].addEventListener("click", function(e){player_click(4);});
+board_element[5].addEventListener("click", function(e){player_click(5);});
+board_element[6].addEventListener("click", function(e){player_click(6);});
+board_element[7].addEventListener("click", function(e){player_click(7);});
+board_element[8].addEventListener("click", function(e){player_click(8);});
+
+let board = [0,0,0,0,0,0,0,0,0];
 let player_turn = true;
 let winner = false;
 let end = false;
@@ -20,20 +29,8 @@ const win_combos = [
     [0,4,8],
     [2,4,6]
 ];
-//board_element_main.addEventListener("click", function(e){if(end == true){board_element_main_click()};});
-board_element[0].addEventListener("click", function(e){player_click(0);});
-board_element[1].addEventListener("click", function(e){player_click(1);});
-board_element[2].addEventListener("click", function(e){player_click(2);});
-board_element[3].addEventListener("click", function(e){player_click(3);});
-board_element[4].addEventListener("click", function(e){player_click(4);});
-board_element[5].addEventListener("click", function(e){player_click(5);});
-board_element[6].addEventListener("click", function(e){player_click(6);});
-board_element[7].addEventListener("click", function(e){player_click(7);});
-board_element[8].addEventListener("click", function(e){player_click(8);});
 function start(){
-    for(let i = 0; i < board_element.length; i++){
-        board_element[i].innerHTML = "";
-    }
+    for(let i = 0; i < board_element.length; i++){board_element[i].innerHTML = "";}
     board = [0,0,0,0,0,0,0,0,0];
     player_turn = true;
     winner = false;
@@ -45,12 +42,10 @@ function player_click(n){
         player_turn = false;
         board[n] = player_symbol;
         board_element[n].innerHTML = "<p>"+player_symbol+"</p>";
-        console.log(board);
-        check_win("player");
+        check_win();
         computer_turn();
     }
 }
-
 function computer_turn(){
     if(player_turn == false && winner == false){
         let computer_move = computer_move_();
@@ -58,79 +53,51 @@ function computer_turn(){
             player_turn = true;
             board[computer_move] = computer_symbol;
             board_element[computer_move].innerHTML = "<p>"+computer_symbol+"</p>";
-            console.log(board);
-            check_win("computer");
+            check_win();
         }else{
             computer_turn();
         }
     }
 }
-
 function computer_move_(){
-    //var win_combos_ = ((0,1),(1,2),(3,4),(4,5),(6,7),(7,8),(0,3),(1,4),(2,5),(3,6),(4,7),(5,8),(0,4),(1,5),(2,6),(3,7),(4,8),(6,4),(8,0))
-    var win_combos_ = [[0,1],[1,2],[3,4],[4,5],[6,7],[7,8],[0,3],[1,4],[2,5],[3,6],[4,7],[5,8],[0,4],[1,5],[2,6],[3,7],[4,8],[6,4],[8,0]];
-    //var win_combos__ = (2,0,5,3,8,6,6,7,8,0,1,2,8,3,4,4,0,2,4)
-    var win_combos__ = [2,0,5,3,8,6,6,7,8,0,1,2,8,3,4,4,0,2,4];
-    
-    var win_combos___ = [0,2,6,8]
-
-    for(let i = 0; i < win_combos_.length; i++){
-        if(board[win_combos_[i][0]] == computer_symbol && board[win_combos_[i][1]] == computer_symbol){
-            if(board[win_combos__[i]] == 0){
-                return win_combos__[i];
-            }
+    for (let i = 0; i < win_combos.length; i++) {
+        var b0 = board[win_combos[i][0]];
+        var b1 = board[win_combos[i][1]];
+        var b2 = board[win_combos[i][2]];
+        if(b0 == b1 && b0 != 0 && b2 == 0){
+            return win_combos[i][2];
+        } else if (b0 == b2 && b0 != 0 && b1 == 0) {
+            return win_combos[i][1];
+        } else if (b1 == b2 && b1 != 0 && b0 == 0) {
+            return win_combos[i][0];
         }
     }
-
-    for(let i = 0; i < win_combos_.length; i++){
-        if(board[win_combos_[i][0]] == player_symbol && board[win_combos_[i][1]] == player_symbol){
-            if(board[win_combos__[i]] == 0){
-                return win_combos__[i];
-            }
-        }
+    if(board[4] == 0){return 4;}
+    if (board[0]==0 || board[2]==0 || board[6]==0 || board[8]==0) {
+        var a = [0,2,6,8]
+        return Math.floor(a[Math.floor(Math.random() * a.length)]);
+    } else {
+        return Math.floor(Math.random() * 9)
     }
-
-    if(board[4] == 0){
-        return 4;
-    }
-
-    var frag = false;
-    for(let i = 0; i < win_combos___.length; i++){
-        if(board[win_combos___[i]] == 0){
-            frag = true;
-        }
-    }
-    if(frag == true){
-        return win_combos___[Math.floor(Math.random() * win_combos___.length)];
-    }
-
-    return Math.floor(Math.random() * 9)
+    return "error";
 }
-
 function check_win(){
     for(let i = 0; i < win_combos.length; i++){
-        if(board[win_combos[i][0]] == player_symbol && board[win_combos[i][1]] == player_symbol && board[win_combos[i][2]] == player_symbol){
-            console.log("player wins");
-            winner = "player";
+        var b0 = board[win_combos[i][0]];
+        var b1 = board[win_combos[i][1]];
+        var b2 = board[win_combos[i][2]];
+        if(b0 == b1 && b0 == b2 && b0 != 0){
             end = true;
-        }
-        if(board[win_combos[i][0]] == computer_symbol && board[win_combos[i][1]] == computer_symbol && board[win_combos[i][2]] == computer_symbol){
-            console.log("computer wins");
-            winner = "computer";
-            end = true;
-        }
-    }
-    if(end == false){
-        end = true;
-        for(let i = 0; i < board.length; i++){
-            if(board[i] == 0){
-                end = false;
+            if(b0 == player_symbol){
+                winner = "player";
+            }else{
+                winner = "computer";
             }
         }
-        if(end == true){
-            console.log("tie");
-            winner = "tie";
-        }
+    }
+    if(board.indexOf(0) == -1 && end == false){
+        end = true;
+        winner = "tie";
     }
     if(end == true){
         if(winner == "player"){
@@ -159,4 +126,3 @@ function board_element_main_click(){
         start();
     }
 }
-
